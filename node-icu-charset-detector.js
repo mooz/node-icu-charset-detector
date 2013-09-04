d0ff74bcab624e77f2bcd2ef22d70b29cdcef1fe
mooz <stillpedant@gmail.com>
2013-09-04 22:04:53 +0900

    Add detectCharsetStream that detects charset of readable streams; Close #3

--- node-icu-charset-detector.js
+++ node-icu-charset-detector.js
@@ -14,5 +14,30 @@ function detectCharset(buffer, options) {
   return charset;
 }
 
+function detectCharsetStream(stream, onDetectionFinish) {
+  var buffer = null;
+  var finished = false;
+
+  function onChunkArrives(chunk) {
+    buffer = buffer ? Buffer.concat(buffer, chunk) : chunk;
+
+    var charset = detectCharset(buffer);
+    if (charset !== null) {
+      // detection succeed. stop reading chunks.
+      stream.removeListener("data", onChunkArrives);
+      finished = true;
+      onDetectionFinish(charset);
+    }
+  }
+
+  stream.on("data", onChunkArrives);
+  stream.on("end", function () {
+    if (!finished) {
+      onDetectionFinish(null);
+    }
+  });
+}
+
 exports.detectCharset = detectCharset;
+exports.detectCharsetStream = detectCharsetStream;
 exports.CharsetMatch = CharsetMatch;
