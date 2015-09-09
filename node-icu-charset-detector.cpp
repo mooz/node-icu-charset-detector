7a5cb0c2568e78ef3d6b1117fa0225b68de5afd2
Masafumi Oyamada <stillpedant@gmail.com>
2015-09-09 21:05:51 +0900

    Support Node.js 4.0 (Use nan 2.0 APIs); Close #20

--- node-icu-charset-detector.cpp
+++ node-icu-charset-detector.cpp
@@ -9,26 +9,28 @@
 #include <cstring>
 #include <iostream>
 
-class CharsetMatch : public node::ObjectWrap
+class CharsetMatch : public Nan::ObjectWrap
 {
 public:
 
     static void
-    Initialize(const v8::Handle<v8::Object> target) {
-        NanScope();
+    RegisterClass(const v8::Handle<v8::Object> target) {
+        Nan::HandleScope scope;
+        const char* ClassName = "CharsetMatch";
 
         v8::Local<v8::FunctionTemplate> constructorTemplate
-            = NanNew<v8::FunctionTemplate>(CharsetMatch::New);
+            = Nan::New<v8::FunctionTemplate>(CharsetMatch::New);
 
+        constructorTemplate->SetClassName(Nan::New(ClassName).ToLocalChecked());
         constructorTemplate->InstanceTemplate()->SetInternalFieldCount(1);
 
         // setup methods
-        NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "getName", CharsetMatch::GetName);
-        NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "getLanguage", CharsetMatch::GetLanguage);
-        NODE_SET_PROTOTYPE_METHOD(constructorTemplate, "getConfidence", CharsetMatch::GetConfidence);
+        Nan::SetPrototypeMethod(constructorTemplate, "getName", CharsetMatch::GetName);
+        Nan::SetPrototypeMethod(constructorTemplate, "getLanguage", CharsetMatch::GetLanguage);
+        Nan::SetPrototypeMethod(constructorTemplate, "getConfidence", CharsetMatch::GetConfidence);
 
         // export class
-        target->Set(NanNew<v8::String>("CharsetMatch"), constructorTemplate->GetFunction());
+        target->Set(Nan::New(ClassName).ToLocalChecked(), constructorTemplate->GetFunction());
     }
 
     CharsetMatch(const char* bufferData, size_t bufferLength) {
@@ -63,71 +65,64 @@ public:
     // JS Constructor
     static
     NAN_METHOD(New) {
-        NanScope();
 
-        if (args.Length() < 1)
-            NanThrowError("Not enough arguments");
+        if (info.Length() < 1)
+            Nan::ThrowError("Not enough arguments");
 
-        v8::Handle<v8::Object> buffer = args[0]->ToObject();
+        v8::Handle<v8::Object> buffer = info[0]->ToObject();
 
         if (!node::Buffer::HasInstance(buffer))
-            NanThrowTypeError("Expected Buffer for the argument");
+            Nan::ThrowTypeError("Expected Buffer for the argument");
 
         try {
-            CharsetMatch::FromBuffer(buffer)->Wrap(args.This()); // under GC
+            CharsetMatch::FromBuffer(buffer)->Wrap(info.This()); // under GC
         } catch (const char* errorMessage) {
-            NanThrowError(errorMessage);
+            Nan::ThrowError(errorMessage);
         }
 
-        NanReturnValue(args.This());
+        info.GetReturnValue().Set(info.This());
     }
 
 private:
     static
     NAN_METHOD(GetName) {
-        NanScope();
-        CharsetMatch* self = node::ObjectWrap::Unwrap<CharsetMatch>(args.This());
+        CharsetMatch* self = Nan::ObjectWrap::Unwrap<CharsetMatch>(info.This());
         if (!self->charsetMatch_) {
-            NanReturnNull();
+            info.GetReturnValue().SetNull();
         }
         UErrorCode icuError = U_ZERO_ERROR;
         const char* detectedCharsetName = ucsdet_getName(self->charsetMatch_, &icuError);
-        NanReturnValue(NanNew<v8::String>(detectedCharsetName, strlen(detectedCharsetName)));
+        info.GetReturnValue().Set(Nan::New<v8::String>(detectedCharsetName, strlen(detectedCharsetName)).ToLocalChecked());
     }
 
     static
     NAN_METHOD(GetLanguage) {
-        NanScope();
-        CharsetMatch* self = node::ObjectWrap::Unwrap<CharsetMatch>(args.This());
+        CharsetMatch* self = Nan::ObjectWrap::Unwrap<CharsetMatch>(info.This());
         if (!self->charsetMatch_) {
-            NanReturnNull();
+            info.GetReturnValue().SetNull();
         }
         UErrorCode icuError = U_ZERO_ERROR;
         const char* detectedLanguage = ucsdet_getLanguage(self->charsetMatch_, &icuError);
-        NanReturnValue(NanNew<v8::String>(detectedLanguage, strlen(detectedLanguage)));
+        info.GetReturnValue().Set(Nan::New<v8::String>(detectedLanguage, strlen(detectedLanguage)).ToLocalChecked());
     }
 
     static
     NAN_METHOD(GetConfidence) {
-        NanScope();
-        CharsetMatch* self = node::ObjectWrap::Unwrap<CharsetMatch>(args.This());
+        CharsetMatch* self = Nan::ObjectWrap::Unwrap<CharsetMatch>(info.This());
         if (!self->charsetMatch_) {
-            NanReturnNull();
+            info.GetReturnValue().SetNull();
         }
         UErrorCode icuError = U_ZERO_ERROR;
         int32_t detectionConfidence = ucsdet_getConfidence(self->charsetMatch_, &icuError);
-        NanReturnValue(NanNew<v8::Number>(static_cast<double>(detectionConfidence)));
+        info.GetReturnValue().Set(Nan::New(static_cast<double>(detectionConfidence)));
     }
 
     UCharsetDetector* charsetDetector_;
     const UCharsetMatch* charsetMatch_;
 };
 
-extern "C"
-void
-init(v8::Handle<v8::Object> target)
-{
-    CharsetMatch::Initialize(target);
+NAN_MODULE_INIT(RegisterModule) {
+    CharsetMatch::RegisterClass(target);
 }
 
-NODE_MODULE(node_icu_charset_detector, init)
+NODE_MODULE(node_icu_charset_detector, RegisterModule);
